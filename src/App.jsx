aec25dedf7b8048a45f18ea881437dd344437886
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2023-09-25 14:19:13 +0600

    props setup in post component

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png" alt="" className="app-header-img" />
       </div>
       <h1>some div here</h1>
-      <Post/>
+      <Post imgUrl ='https://cdn.pixabay.com/photo/2017/10/17/16/10/fantasy-2861107_1280.jpg' userName="Bahadur mia" caption="caption heree"/>
     </div>
   )
 }
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -2,21 +2,22 @@ import React from 'react'
 import { Avatar } from '@mui/material'
 import './post.css'
 import im1 from '../../images/img-1.jpg'
+import profile from '../../images/img-3.jpg'
 
-const Post = () => {
+const Post = ({imgUrl, userName, caption}) => {
   return (
     <div className='post'>
       <div className="post-header">
          <Avatar>
-            B
+            <img src={profile} alt="" className='profile-im' />
          </Avatar>
-         <h3 className='userName'>Bahadur</h3>
+         <h3 className='userName'>{userName}</h3>
       </div>
       <div className="post-image">
-        <img src={im1} alt=""  className='post-img-image'/>
+        <img src={imgUrl} alt=""  className='post-img-image'/>
       </div>
       <div className="post-text">
-        <h4> <strong>Bahadur</strong> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint, atque?</h4>
+        <h4> <strong>Bahadur</strong> {caption}</h4>
       </div>
     </div>
   )
