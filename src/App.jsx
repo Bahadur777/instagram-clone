647cc3f9969db3b90ff696751781773a4bbacb4d
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2023-09-27 19:37:30 +0600

    id problem not be solve

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import Post from './components/post/Post'
+import { db } from './firebase';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 const App = () => {
   const [post, setPosts] = useState([
@@ -15,9 +18,18 @@ const App = () => {
       caption:"caption is here",
       imgUrl:"https://cdn.pixabay.com/photo/2013/05/11/08/28/sunset-110305_640.jpg"
     },
-  ])
+    
+  ]);
+  useEffect(() =>{
+    db.collection('post').onSnapshot(onSnapshot =>{
+      setPosts(onSnapshot.docs.map(doc =>({
+                id: doc.data(),
+                ...doc.data()
+      })));
+     })
+  },[]);
   return (
-    <div className="app">
+    <div className="app" key={post.id}>
       <div className="app-header">
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png" alt="" className="app-header-img" />
       </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,18 @@
-import firebase from "firebase"
-const firebaseApp = firebase.initializeApp({
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+// const firebaseApp = firebase.initializeApp({
+//     apiKey: "AIzaSyCjOATTS-hdPVkypGEAScEXHmCfMLMKxes",
+//     authDomain: "instagram-clone-e1445.firebaseapp.com",
+//     projectId: "instagram-clone-e1445",
+//     storageBucket: "instagram-clone-e1445.appspot.com",
+//     messagingSenderId: "111688823554",
+//     appId: "1:111688823554:web:eca53cb357682bf54b081b",
+//     measurementId: "G-2W7Y4K2CLS"
+// })
+
+const firebaseConfig = {
     apiKey: "AIzaSyCjOATTS-hdPVkypGEAScEXHmCfMLMKxes",
     authDomain: "instagram-clone-e1445.firebaseapp.com",
     projectId: "instagram-clone-e1445",
@@ -7,10 +20,13 @@ const firebaseApp = firebase.initializeApp({
     messagingSenderId: "111688823554",
     appId: "1:111688823554:web:eca53cb357682bf54b081b",
     measurementId: "G-2W7Y4K2CLS"
-})
+  };
+// const db = firebaseApp.firestore();
+// const auth = firebase.auth();
+// const storage = firebase.storage();
 
-const db = firebaseApp.firestore();
-const auth = firebase.auth();
-const storage = firebase.storage();
-
-export{db, auth, storage}
+const firebaseApp = initializeApp(firebaseConfig);
+// Access Firebase services
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
+export {db, auth}
